1bdde11a8dc647dca525c7d931c34bc52d23d6c1
bhart <brandon.hart@adtran.com>
2018-12-07 16:34:46 -0600

    Refactor puzzle 1

--- Puzzle-1/solution.py
+++ Puzzle-1/solution.py
@@ -1,16 +1,12 @@
-
-
 def solve():
     frequencies = [0]
     frequency = 0
-    run = 1
-    while run:
+    while True:
         with open("test_data.txt", "r") as test_data:
             for each in test_data:
                 frequency += int(each)
                 if frequency in frequencies:
                     print(frequency)
-                    run = 0
                     exit()
                 else:
                     frequencies.append(frequency)
